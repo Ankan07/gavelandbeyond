fd8c02a28711260bce7c60cd03bfe0bd218a94bf
Ankan Mukherjee <ankantech96@gmail.com>
2018-11-02 22:26:21 +0530

    something is working

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express=require('express');
 const app=express();
 
 
+var path= require("path");
 
 app.set('view engine','ejs');
+app.use(express.static('public'));
 
 app.get('/',function(req,res){
        
-    res.sendFile('homepage.html');
+    res.sendFile(path.join(__dirname+'/homepage.html'))
 
 });
 app.listen('3000',()=>{
