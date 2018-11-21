150f9a870913c45e2fd00fcebfc3ef24d7ba3786
Ankan Mukherjee <ankantech96@gmail.com>
2018-11-21 18:12:24 +0530

    comments page done

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
 const express=require('express');
 const app=express();
-
+var bodyParser = require('body-parser');
 
 var path= require("path");
+const admin = require('firebase-admin');
+
+var serviceAccount = require('./gb.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+var db = admin.firestore();
+db.settings({
+    timestampsInSnapshots: true
+  });
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
        
     res.sendFile(path.join(__dirname+'/homepage.html'))
 
 });
+app.get('/article/:id',(req,res)=>{
+var articleid=req.params.id;
+console.log(articleid.toString());
+    var Ref = db.collection('home-page').doc(articleid.toString());
+    var getDoc = Ref.get()
+      .then(doc => {
+        if (!doc.exists) {
+          console.log('No such document!');
+          res.render('article.ejs',{data:"ERROR 404 NOT FOUND",pictureurl:""});
+        } else {
+          // console.log('Document data:', doc.data());
+  res.render('article.ejs',{data:doc.data().subtitle,pictureurl:doc.data().pictureurl,aid:articleid});
+        }
+      })
+      .catch(err => {
+        console.log('Error getting document', err);
+      });
+  
+})
+  
+
 app.listen('3000',()=>{
 
 console.log("app listening on 3000");
--- public/backendhome.js
+++ public/backendhome.js
@@ -11,14 +11,14 @@
 // });
 
 // var db = admin.firestore();
-var config = {
-  apiKey: "AIzaSyAIECX7nUeA6W6Z52kvWx25GeZ4OfIknAo",
-  authDomain: "gavel-and-beyond.firebaseapp.com",
-  projectId: "gavel-and-beyond",
-  databaseURL: "https://gavel-and-beyond.firebaseio.com",
-  storageBucket: "gavel-and-beyond.appspot.com",
-};
-firebase.initializeApp(config);
+// var config = {
+//   apiKey: "AIzaSyAIECX7nUeA6W6Z52kvWx25GeZ4OfIknAo",
+//   authDomain: "gavel-and-beyond.firebaseapp.com",
+//   projectId: "gavel-and-beyond",
+//   databaseURL: "https://gavel-and-beyond.firebaseio.com",
+//   storageBucket: "gavel-and-beyond.appspot.com",
+// };
+// firebase.initializeApp(config);
 
 
  
@@ -39,7 +39,7 @@ var allarticles = homepages.get()
      console.log(doc.id, '=>', doc.data());
 
     renderarticle(doc);
-    renderimage(doc);
+    // renderimage(doc);
    
     });
   })
@@ -51,41 +51,45 @@ var allarticles = homepages.get()
   var oc=document.getElementById('oc');
   //
   function renderarticle(doc){
+    var link=document.createElement('a');
+    link.setAttribute("href","/article/"+doc.id);
+
+
     var column = document.createElement("div");
-    column.setAttribute('class', 'col s12 m5 l3');
+    column.setAttribute('class', 'col s12 m5 l4');
     var card=document.createElement("div");
     card.setAttribute('class','card hoverable');
   
-    var cardimage=document.createElement("div");
-    cardimage.setAttribute('class','card-image waves-effect waves-block waves-light');
+    // var cardimage=document.createElement("div");
+    // cardimage.setAttribute('class','card-image waves-effect waves-block waves-light');
  
-    var image=document.createElement('img');
-    image.setAttribute('src',doc.data().pictureurl);
-    //image.setAttribute('class', 'responsive-img');
+    // var image=document.createElement('img');
+    // image.setAttribute('src',doc.data().pictureurl);
+    // //image.setAttribute('class', 'responsive-img');
     
-    image.setAttribute('class',"thumbnail activator");
+    // image.setAttribute('class',"thumbnail activator");
     
    
      
-    cardimage.appendChild(image);
+    // cardimage.appendChild(image);
     var cardcontent=document.createElement("div");
     cardcontent.setAttribute('class','card-content');
     var cardaction=document.createElement('div');
      cardaction.setAttribute('class','card-action');
      var span = document.createElement("SPAN");
-     span.setAttribute('class','card-title ');
+     span.setAttribute('class','card-title cardtitle');
      var i=document.createTextNode(doc.data().title);
      span.appendChild(i);
-     span.setAttribute('class',"truncate")
+    // span.setAttribute('class',"truncate")
      var para= document.createElement("P");
-     var t = document.createTextNode(doc.data().subtitle);
-     para.setAttribute("class","truncate");     // Create a text node
+     var t = document.createTextNode(doc.data().subtitle.substring(0,200)+"...");
+     para.setAttribute("class","cardpara");     // Create a text node
 para.appendChild(t); 
 cardcontent.appendChild(span);
 cardcontent.appendChild(para);
 var createA = document.createElement('a');
 var createAText = document.createTextNode('Read Article');
-createA.setAttribute('href', "http://www.google.com");
+createA.setAttribute('href', "/article/"+doc.id);
 createA.appendChild(createAText);
 cardaction.appendChild(createA);
 // var cardreveal=document.createElement("div");
@@ -108,17 +112,18 @@ cardaction.appendChild(createA);
 
 
 
-     card.appendChild(cardimage);
+   //  card.appendChild(cardimage);
 
      card.appendChild(cardcontent);
      card.appendChild(cardaction);
  column.appendChild(card);
-row.appendChild(column);
+ link.appendChild(column);
+row.appendChild(link);
 
-
+// var cm=document.createElement("p");
+// var t=document.createTextNode(doc.data().subtitle);
+// cm.appendChild(t);
+// var v=document.getElementById("oc");
+// v.appendChild(cm);
   } 
-  function renderimage(doc){
-  var cm=document.getElementById("1");
-  cm.setAttribute("src",doc.data().pictureurl);
-
-  }
+ 
